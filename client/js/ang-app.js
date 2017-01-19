e71d9667006e8fa881f1a9b530b0052f7d4c6796
x148128 <rana.das@hrblock.com>
2017-01-19 15:30:54 +0000

    Add angular-ui-router denencency inject  ui.router to configue ruotes. Use $stateProvider to view partials

--- client/js/ang-app.js
+++ client/js/ang-app.js
@@ -1,3 +1,33 @@
 'use strict';
 
-var gtcAngularApp = angular.module('gtcAngular', []);
+var gtcAngularApp = angular.module('gtcAngular', ['ui.router']);
+
+var routesConfig = function RoutesConfig($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('page1');
+
+    $stateProvider
+        .state('page1', {
+            url: '/page1',
+            templateUrl: 'partials/page1.html',
+            controller: Page1Controller,
+            controllerAs: 'vm'
+        })
+        .state('page2', {
+            url: '/page2',
+            templateUrl: 'partials/page2.html'
+        })
+        .state('page3', {
+            url: '/page3',
+            templateUrl: 'partials/page3.html'
+        })
+    ;
+};
+routesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+gtcAngularApp.config(routesConfig);
+
+
+function Page1Controller() {
+    console.log('Page1 Controller ');
+    var self = this;
+    self.pageTitle = "Page One";
+}

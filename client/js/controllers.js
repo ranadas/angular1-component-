2dba719398881a3ea9087717c3eebe79ef5d6bfc
x148128 <rana.das@hrblock.com>
2017-01-25 16:48:47 +0000

    Adding ngMessages to a new search Form

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -40,11 +40,23 @@ function Page3Controller($location, service1) {
     console.log('Page3  Controller ');
     var self = this;
     self.pageTitle = "Page Three!!";
-
+    self.searchPattern = /^[a-zA-Z0-9]*$/;
     self.goToLink = function (url) {
         console.log('Navigating to '+url);
         service1.login();
         $location.path(url);
+    };
+
+    self.submitForm = function(isValid) {
+        console.log('User clicked submit with :', self.user);
+        if (isValid) {
+            console.log('Submit form success, navigating to page3.');
+            $location.path('/page3');
+        }
+    };
+
+    self.makeSearch = function(isValid) {
+        console.log('Search in page3.');
     }
 }
 Page3Controller.$inject = ['$location', 'aTypicalService'];

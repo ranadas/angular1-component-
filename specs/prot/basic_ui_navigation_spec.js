ef622aef717af1bb3e1ca1abae54b785c8453c7b
x148128 <rana.das@hrblock.com>
2017-01-25 17:19:33 +0000

    Fix title message to assert

--- specs/prot/basic_ui_navigation_spec.js
+++ specs/prot/basic_ui_navigation_spec.js
@@ -7,13 +7,13 @@ describe('tests button click in order to navigate from page1, page2 & page3', fu
 
     it('should activate ng-click on pages[1-3]  and asserting the label.', function () {
         element(by.buttonText('Go to Page2')).click();
-        expect(element(by.binding('vm.pageTitle')).getText()).toBe('Page Two!!');
+        expect(element(by.binding('vm.pageTitle')).getText()).toBe('This is Page Two!!');
 
         element(by.buttonText('Go to Page3')).click();
-        expect(element(by.binding('vm.pageTitle')).getText()).toBe('Page Three!!');
+        expect(element(by.binding('vm.pageTitle')).getText()).toBe('This is Page Three!!');
         
         element(by.buttonText('Back to Page1')).click();
-        expect(element(by.binding('vm.pageTitle')).getText()).toBe('Page One');
+        expect(element(by.binding('vm.pageTitle')).getText()).toBe('This is Page One');
     });
 });
 
@@ -25,7 +25,7 @@ describe('tests button click in order to navigate from page2 to page3', function
 
     it('should activate ng-click on pages[1-3]  and asserting the label.', function () {
         element(by.buttonText('Go to Page3')).click();
-        expect(element(by.binding('vm.pageTitle')).getText()).toBe('Page Three!!');
+        expect(element(by.binding('vm.pageTitle')).getText()).toBe('This is Page Three!!');
     });
 });
 
@@ -36,6 +36,6 @@ describe('tests button click in order to navigate from page2 to page3', function
 
     it('should activate ng-click on pages[1-3]  and asserting the label.', function () {
         element(by.buttonText('Back to Page1')).click();
-        expect(element(by.binding('vm.pageTitle')).getText()).toBe('Page One');
+        expect(element(by.binding('vm.pageTitle')).getText()).toBe('This is Page One');
     });
 });
